eaff7eafda2def3146960695242e8981aeee70f3
Michael Flaxman <mflaxman@gmail.com>
2014-11-07 22:37:52 -0800

    fix static file serving and revert procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn blockexplorer.wsgi --log-file -
+web: newrelic-admin run-program gunicorn blockexplorer.wsgi
--- blockexplorer/wsgi.py
+++ blockexplorer/wsgi.py
@@ -7,8 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+# http://raven.readthedocs.org/en/latest/config/django.html#wsgi-middleware
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blockexplorer.settings")
 
+from dj_static import Cling
+
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = Sentry(Cling(get_wsgi_application()))
