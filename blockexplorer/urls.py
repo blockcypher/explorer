184c97249f773955356ab14c9b40f8f889144e92
Michael Flaxman <mflaxman@gmail.com>
2014-11-13 01:08:15 -0800

    update urls.py for coin overview

--- blockexplorer/urls.py
+++ blockexplorer/urls.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 urlpatterns = patterns('',
 
     url(r'^$', 'homepage.views.home', name='home'),
-    url(r'(?P<coin_symbol>[-\w]+)/latest-block$', 'blocks.views.latest_block', name='latest_block'),
-    url(r'(?P<coin_symbol>[-\w]+)/address/(?P<address>\w+)$', 'addresses.views.address_overview', name='address_overview'),
-    url(r'(?P<coin_symbol>[-\w]+)/tx/(?P<tx_hash>\w+)$', 'transactions.views.transaction_overview', name='transaction_overview'),
-    url(r'(?P<coin_symbol>[-\w]+)/block/(?P<block_representation>\w+)$', 'blocks.views.block_overview', name='block_overview'),
+    url(r'(?P<coin_symbol>[-\w]+)/latest-block/$', 'blocks.views.latest_block', name='latest_block'),
+    url(r'(?P<coin_symbol>[-\w]+)/address/(?P<address>\w+)/$', 'addresses.views.address_overview', name='address_overview'),
+    url(r'(?P<coin_symbol>[-\w]+)/tx/(?P<tx_hash>\w+)/$', 'transactions.views.transaction_overview', name='transaction_overview'),
+    url(r'(?P<coin_symbol>[-\w]+)/block/(?P<block_representation>\w+)/$', 'blocks.views.block_overview', name='block_overview'),
+    url(r'(?P<coin_symbol>[-\w]+)/$', 'homepage.views.coin_overview', name='coin_overview'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
