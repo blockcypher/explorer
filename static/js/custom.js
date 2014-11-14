dc77d4124c5c79faf6c75343c33b9751c9810c0a
Amelia <michliga@gmail.com>
2014-11-14 12:46:16 -0800

    Adds filters to search for address, txn, block, etc.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,33 @@
-function setCoin(coin){
+function setCoin(coin) {
   $('#coin_symbol').val(coin);
 }
+function filterSearch(filter, that) {
+  //Update search input
+  var sPlaceholder = '';
 
+  switch(filter) {
+    case 'address':
+      sPlaceholder = '16Fg2yjwrbtC6fZp61EV9mNVKmwCzGasw5';
+      break;
+    case 'tx_hash':
+      sPlaceholder = '2509e5b65ed362557fcf2104e89f3c2430ceecc6a3275556c1b966eb641fe092';
+      break;
+    case 'block_hash':
+      sPlaceholder = '0000000000000000001e847e71b955482dab7228f4849a4659c0cf5cf323f247';
+      break;
+    case 'block_num':
+      sPlaceholder = '330,027';
+      break;
+    default:
+      sPlaceholder = '';
+  }
+
+  $('#search_string').val(sPlaceholder);
+  $('#search_filter').val(filter);
+
+  $('.search-filter .active').removeClass('active');
+  $(that).addClass('active');
+}
 function satoshis_to_btc(satoshis) {
   // Round to 4 decimal places
   var btc = satoshis/(Math.pow(10,8));
