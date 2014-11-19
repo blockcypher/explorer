e53f224598adfe14a297008b88a93b0ea4c3c8cb
Michael Flaxman <mflaxman@gmail.com>
2014-11-19 11:51:03 -0800

    Fixes #38

--- addresses/templatetags/btc_formats.py
+++ addresses/templatetags/btc_formats.py
@@ -11,6 +11,11 @@ def satoshis_to_btc_rounded(satoshis):
     return satoshis_to_btc(satoshis=satoshis, decimals=4)
 
 
+@register.filter(name='satoshis_to_bitcoin')
+def satoshis_to_bitcoin(satoshis):
+    return satoshis_to_btc(satoshis=satoshis)
+
+
 @register.filter(name='coin_symbol_to_display_name')
 def coin_symbol_to_display_name(coin_symbol):
     return COIN_SYMBOL_MAPPINGS[coin_symbol]['display_name']
