259ac0edc9e8c9240611c495bdd89c169def47c6
Matthieu Riou <matthieu@blockcypher.com>
2020-06-05 11:38:47 -0700

    Plural typo

--- addresses/templatetags/btc_formats.py
+++ addresses/templatetags/btc_formats.py
@@ -15,7 +15,7 @@ def satoshis_to_user_units_trimmed(input_satoshis, user_unit='btc', coin_symbol=
     # fix for coinbase input
     if not isinstance(input_satoshis, int):
         return ""
-    input_type = 'satoshis' if coin_symbol != 'eth' else 'wei'
+    input_type = 'satoshi' if coin_symbol != 'eth' else 'wei'
     return format_crypto_units(
             input_quantity=input_satoshis,
             input_type=input_type,
